aa835de77fde85b3e1cde6b373b0bfde9baf8737
Omkar Patil <git21cs072-t@students.git.edu>
2024-08-25 23:32:07 +0530

    strings Done!!

---
+++ 2.Data Types/script.js
@@ -0,0 +1,23 @@
+console.log('Lets Start Data Types Section');
+
+//==================> Strings <===================
+//types
+//1. Single Quotes
+let sQuote = 'Single Quotes';
+//2.Double Quotes
+let dQuote = 'Double Quotes';
+//3.Backticks
+let name = 'Anisha';
+let bekTiks = `my Gf name is ${name}`;
+console.log(bekTiks);
+
+//any javascript Code can be written in that Bracket
+
+//ex function Call
+function namee() {
+  return 'Anisha and Dishita';
+}
+let gfName = `My Gf name is ${namee()}`;
+console.log(gfName);
+
+//===================> Numbers <===================
