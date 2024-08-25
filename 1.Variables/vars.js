d80fe2d61141702bda24668e6d83de99858105f0
Omkar Patil <git21cs072-t@students.git.edu>
2024-08-25 23:11:50 +0530

    Done vars and Let and Const Terminology !!

---
+++ 1.Variables/vars.js
@@ -0,0 +1,23 @@
+console.log('Welcome to Variables Section');
+
+//variable Declaration
+
+var x = 'om namah Shivay';
+var y = 12.4;
+var z = 45565555656;
+console.log(x);
+console.log(y);
+console.log(z);
+
+//after es 6 we have two more ways to declare variables
+//we prefer let and const over var
+
+//consrt can not be reassigned
+const a = 5;
+// a = 12; //
+// console.log(a); error
+
+let p = 10;
+console.log(p);
+p = 666;
+console.log(p); // reassigned Value Will be Printed
