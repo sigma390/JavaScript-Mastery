ee0bbcf04516aadf37cdd7af94929bcf9c3aa4bb
Omkar Patil <git21cs072-t@students.git.edu>
2024-08-31 13:46:28 +0530

    If statements Done!!!

---
+++ 4.Program Control Flow/script.js
@@ -0,0 +1,11 @@
+//====================> IF STATEMENT <====================
+
+let age = 18;
+
+if (age > 18) {
+  console.log('You are eligible to vote');
+} else if (age == 18) {
+  console.log('You are just 18 years old');
+} else {
+  console.log('Grow Up Kiddo!!');
+}
