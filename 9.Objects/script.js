331ef8a339524b169c3ea0e011468316fb45561f
Omkar Patil <git21cs072-t@students.git.edu>
2024-09-22 10:24:45 +0530

    Objects Started!!!!!

---
+++ 9.Objects/script.js
@@ -0,0 +1,13 @@
+//=======================> Objects <=======================
+
+let person = {
+  firstName: 'Om',
+  lastName: 'Patil',
+  age: 23,
+  city: 'Kolhapur',
+  car: {
+    brand: 'Nissan',
+    model: 'GTR',
+    year: 2017,
+  },
+};
