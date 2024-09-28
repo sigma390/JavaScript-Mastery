56b672feb5596485f0f8dc2ac9b86390cde3f7c3
Omkar Patil <git21cs072-t@students.git.edu>
2024-09-28 13:39:08 +0530

    this keyword

---
+++ 12.Claasses/Script.js
@@ -0,0 +1,18 @@
+// new Keyword
+
+let myCar = new Object({ name: 'pp', year: 23 });
+console.log(myCar);
+
+//extensivly Used for Date Objects
+
+const date = new Date();
+console.log(date);
+console.log(date.getFullYear());
+
+//shorthand
+
+const obj = {}; // create Object
+
+//similarliy for arrays
+const arr = [];
+//or const arr = new Array(1,2,3); same
