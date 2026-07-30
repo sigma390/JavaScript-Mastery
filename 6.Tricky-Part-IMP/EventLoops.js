c7c4c94ddaa7e61641225b1870728e66b7865c30
sigma390 <sigmacoder390@gmail.com>
2026-07-30 18:45:34 +0530

    Enhance EventLoops.js with detailed promise task examples and add practice.js for asynchronous function demonstrations

--- 6.Tricky-Part-IMP/EventLoops.js
+++ 6.Tricky-Part-IMP/EventLoops.js
@@ -21,3 +21,53 @@ Promise.resolve().then(() => console.log("second Promise F"));
 console.log("G")
 
 // A , G , Promise Resolved D  , Second Promise F , Inside Promise E (BEACUSE ITS scheduled at 0 ms if it was more than that of B then it would have been in Queue after B) , B, C.....
+
+
+
+
+//always thibk like a queue FIFO
+
+console.log("A");
+
+Promise.resolve().then(() => { //task 1
+    console.log("B");
+
+    Promise.resolve().then(() => { //task 4
+        console.log("C");
+    });
+
+    Promise.resolve().then(() => { //task 5
+        console.log("D");
+
+        Promise.resolve().then(() => { //task 7
+            console.log("E");
+        });
+
+        console.log("F");
+    });
+
+    console.log("G");
+});
+
+Promise.resolve().then(() => { //task 2
+    console.log("H");
+
+    Promise.resolve().then(() => { //task 6
+        console.log("I");
+
+        Promise.resolve().then(() => { //task 8
+            console.log("J");
+        });
+
+        console.log("K");
+    });
+
+    console.log("L");
+});
+
+Promise.resolve().then(() => {  //task 3
+    console.log("M");
+});
+
+console.log("N");
+
---
+++ 6.Tricky-Part-IMP/practice.js
@@ -0,0 +1,1 @@
+//mechanism of js to achieve asynchronousity by moving callbacks from task queue tp the call stack 
