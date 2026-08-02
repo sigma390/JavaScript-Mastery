b367c5a88597da5a6fc5916610578b95eaed3386
sigma390 <sigmacoder390@gmail.com>
2026-08-02 18:21:37 +0530

    fix: correct user assignment in auth middleware after token verification

--- Node-Express/src/middlewares/index.ts
+++ Node-Express/src/middlewares/index.ts
@@ -7,9 +7,10 @@ export const auth = (req: Request, res: Response, next: NextFunction) => {
     const token = req.headers?.authorization?.split(" ")[1]
     if (token) {
         const decoded = jwt.verify(token, "SECRET")
+        req.user = decoded;
     }
 
-    req.user = decoded;
+
     next()
 
 
